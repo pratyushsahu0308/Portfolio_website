12a623885aaff149913c78f820d4d6cc9bcbb2fb
pratyushsahu0308 <65818115+pratyushsahu0308@users.noreply.github.com>
2023-03-22 15:14:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let text = document.getElementById("text");
 window.addEventListener("scroll", function () {
     let value = this.window.scrollY;
-    text.style.marginRight = (value * 5) + "px";
+    text.style.marginLeft = -(value * 5) + "px";
 })
