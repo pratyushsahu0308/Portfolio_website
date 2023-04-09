9ec94c4d39bd4779f6ef9603f015e1a23c6d49a3
pratyushsahu0308 <65818115+pratyushsahu0308@users.noreply.github.com>
2023-04-09 17:50:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 let text = document.getElementById("text");
 let intro1 = document.getElementById("intro1");
 let square = document.getElementById("square");
+
 let menuIcon = document.querySelector('#menu-icon');
 let navBar = document.querySelector('.navbar');
 let logo = document.querySelector('.logo');
 const media = window.matchMedia('(min-width: 701px)');
+
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-thin');
     menuIcon.classList.toggle('fa-caret-down');
@@ -14,11 +16,15 @@ menuIcon.onclick = () => {
     text.classList.toggle('up');
     text.style.transition = "all .5s";
 }
+
 window.addEventListener("scroll", function () {
     let value = this.window.scrollY;
     text.style.marginLeft = -(value * 1.5) + "px";
     text.style.marginTop = -(value * 2) + "px";
+
+
 })
+
 document.querySelector('body').addEventListener('mousemove', eyeball);
 function eyeball() {
     const eye = document.querySelectorAll('.eye');
@@ -38,3 +44,7 @@ const sr = ScrollReveal({
 sr.reveal('.square', { delay: 350, origin: 'right' });
 sr.reveal('.ski,.shadow', { delay: 350, origin: 'bottom' });
 sr.reveal('.contact', { delay: 200, origin: 'bottom' });
+window.addEventListener("scroll", function () {
+    var top = document.getElementById("top");
+    top.classList.toggle("active", window.scrollY > 300)
+})
